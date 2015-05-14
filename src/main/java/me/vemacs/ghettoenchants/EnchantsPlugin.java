065b7ff381871b694cdbc42cdcc3765233bc7b15
vemacs <gentoo@outlook.com>
2015-05-14 16:09:53 -0600

    let's be clean

--- src/main/java/me/vemacs/ghettoenchants/EnchantsPlugin.java
+++ src/main/java/me/vemacs/ghettoenchants/EnchantsPlugin.java
@@ -1,7 +1,6 @@
 package me.vemacs.ghettoenchants;
 
 import com.google.common.base.Joiner;
-import com.google.common.collect.ImmutableList;
 import lombok.Getter;
 import me.vemacs.ghettoenchants.enchants.BaseEnchant;
 import me.vemacs.ghettoenchants.enchants.pickaxe.DemoPickEnchant;
@@ -17,6 +16,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class EnchantsPlugin extends JavaPlugin {
@@ -26,13 +26,13 @@ public class EnchantsPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         instance = this;
-        List<Class<? extends BaseEnchant>> toRegister = new ImmutableList.Builder<Class<? extends BaseEnchant>>()
-                .add(DemoPickEnchant.class)
-                .add(ExplodePickEnchant.class)
-                .add(LaserPickEnchant.class)
-                .add(FireworkPickEnchant.class)
-                .add(SmiteEnchant.class)
-                .build();
+        List<Class<? extends BaseEnchant>> toRegister = Arrays.asList(
+                DemoPickEnchant.class,
+                ExplodePickEnchant.class,
+                FireworkPickEnchant.class,
+                LaserPickEnchant.class,
+                SmiteEnchant.class
+        );
         for (Class<? extends BaseEnchant> c : toRegister) {
             try {
                 BaseEnchant enc = EnchantUtils.newInstance(c);
